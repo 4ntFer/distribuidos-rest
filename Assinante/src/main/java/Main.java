e332d1ef674ec8b75725bb9ab2073481088d1941
Lucas Walger <walger.profissional@gmail.com>
2025-04-21 00:30:33 -0300

    Assinante e Marketing mudaram de forma, tendo mais opções

--- Assinante/src/main/java/Main.java
+++ Assinante/src/main/java/Main.java
@@ -1,45 +1,48 @@
 import com.rabbitmq.client.*;
 
 import java.nio.charset.StandardCharsets;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
-    private final static String ADV_X_EXC = "AdvertiseX";
-    private final static String ADV_Y_EXC = "AdvertiseY";
+    private final static String ADV_EXC = "Advertisement";
+    private final static String ADV_TOPIC = "location";
+    private final static String[] locations = {"Porto de Galinhas","Paranagua","Rio de Janeiro","Belém"};
+    private final static String[] codes = {"BR.PE.porto_de_galinhas","BR.PR.paranagua","BR.RJ.rio_de_janeiro","BR.PA.belem"};
 
     public static void main(String[] args) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
-        try(Connection connection = factory.newConnection();
-            Channel channel = connection.createChannel()) {
-            channel.exchangeDeclare(ADV_X_EXC, "fanout");
-            channel.exchangeDeclare(ADV_Y_EXC, "fanout");
-            System.out.println("Write X to receive offers from X, Y to receive offers from Y, A to both");
-            int character = System.in.read();
-            String queue = channel.queueDeclare().getQueue();
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.exchangeDeclare(ADV_EXC, "topic");
 
-            switch (character)
-            {
-                case 'X':
-                    channel.queueBind(queue, ADV_X_EXC, "");
-                    break;
-                case 'Y':
-                    channel.queueBind(queue, ADV_Y_EXC, "");
-                    break;
-                default:
-                    channel.queueBind(queue, ADV_X_EXC, "");
-                    channel.queueBind(queue, ADV_Y_EXC, "");
-            }
+        String queue = channel.queueDeclare().getQueue();
 
+        DeliverCallback deliverCallback = (_, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            System.out.println("--> Oferta Recebida: " + message);
+        };
+        channel.basicConsume(queue, true, deliverCallback, _ -> {});
 
-
-            DeliverCallback deliverCallback = (_, delivery) -> {
-                String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
-                System.out.println("Offer Received: " + message);
-            };
-            channel.basicConsume(queue, true, deliverCallback, _ -> {});
-            while(System.in.available()>0)
-                System.in.read();
-            System.in.read();
+        System.out.println("** Escolha quais anúncios escutar:");
+        for(int i = 0; i<locations.length;i++)
+        {
+            System.out.println("["+i+"] " + locations[i]);
         }
+        boolean flag_run = true;
+        while(flag_run) {
+            Scanner scanner = new Scanner(System.in);
+            int id = scanner.nextInt();
+            if(id<0 || id>= locations.length){
+                flag_run = false;
+            } else {
+                channel.queueBind(queue, ADV_EXC, ADV_TOPIC + "." + codes[id]);
+                System.out.println("** ANÚNCIOS DE " + ADV_EXC + "(topic: " + ADV_TOPIC + "." + codes[id] + ") SERÃO ESCUTADOS");
+            }
+        }
+
     }
 }
--- Marketing/src/main/java/Main.java
+++ Marketing/src/main/java/Main.java
@@ -5,11 +5,15 @@ import com.rabbitmq.client.Channel;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
+
 public class Main {
-    private final static String ADV_X_EXC = "AdvertiseX";
-    private final static String ADV_Y_EXC = "AdvertiseY";
+    private final static String ADV_EXC = "Advertisement";
+    private final static String ADV_TOPIC = "location";
+    private final static String[] locations = {"Porto de Galinhas","Paranagua","Rio de Janeiro","Belém"};
+    private final static String[] codes = {"BR.PE.porto_de_galinhas","BR.PR.paranagua","BR.RJ.rio_de_janeiro","BR.PA.belem"};
 
     public static void main(String[] args) throws Exception {
         //START RABBIT MQ QUEUES
@@ -18,34 +22,32 @@ public class Main {
         try(Connection connection = factory.newConnection();
             Channel channel = connection.createChannel()){
 
-            channel.exchangeDeclare(ADV_X_EXC,"fanout");
-            channel.exchangeDeclare(ADV_Y_EXC,"fanout");
+            channel.exchangeDeclare(ADV_EXC,"topic");
             int info;
 
             //LOOP TO SEND TO MARKETING
-            do {
-                String message;
-                String exchange;
+            boolean flag_run = true;
+            while(flag_run) {
 
-                System.out.println("X to advertise to X cruise, Y to advertise to Y cruise, N to stop process.");
-                info = System.in.read();
 
-                switch (info) {
-                    case 'X':
-                        exchange = ADV_X_EXC;
-                        message = "Cruise X Offer "+ThreadLocalRandom.current().nextInt(0, 101)+"% off - " + LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
-                        break;
-                    case 'Y':
-                        exchange = ADV_Y_EXC;
-                        message = "Cruise Y Offer "+ThreadLocalRandom.current().nextInt(0, 101)+"% off - " + LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
-                        break;
-                    default:
-                        continue;
 
+                System.out.println("Escolha um Lugar para Anunciar:");
+                for(int i = 0; i<locations.length;i++)
+                {
+                    System.out.println("["+i+"]" + locations[i]);
                 }
-                channel.basicPublish(exchange,"",null,message.getBytes(StandardCharsets.UTF_8));
-                System.out.println("SENT TO " + exchange + " MESSAGE:\"" + message+"\"");
-            } while (info!= 'N');
+
+                Scanner scanner = new Scanner(System.in);
+                int id = scanner.nextInt();
+                if(id<0 || id>= locations.length){
+                    flag_run = false;
+                } else {
+                    String message = "Cruseiro saindo de " + locations[id] + " | Oferta: " + ThreadLocalRandom.current().nextInt(0, 101) + "% off - " + LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+
+                    channel.basicPublish(ADV_EXC, ADV_TOPIC + "." + codes[id], null, message.getBytes(StandardCharsets.UTF_8));
+                    System.out.println("SENT TO " + ADV_EXC + "(topic: " + ADV_TOPIC + "." + codes[id] + ")" + " MESSAGE:\"" + message + "\"");
+                }
+            }
         }
     }
 }
